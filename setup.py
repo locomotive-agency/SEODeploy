11848b3178cbe8e096911b8b261744d7fe44442a
JR Oakes <jroakes@gmail.com>
2020-04-23 08:38:57 -0400

    updated package

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+from codecs import open
+
+exec(open('gtts/version.py').read())
+
+setup(
+    version=__version__,   # type: ignore # noqa: F821
+    test_suite='cktesting.tests',
+)
