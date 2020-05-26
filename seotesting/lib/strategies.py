f0258b340acf458ae19a6e8891dbd4e8dbcfe60b
JR Oakes <jroakes@gmail.com>
2020-05-26 10:41:03 -0400

    Updated COmparision Strategies

--- seotesting/lib/strategies.py
+++ seotesting/lib/strategies.py
@@ -26,6 +26,8 @@ import os
 import sys
 import importlib
 
+from dictdiffer import diff
+
 from .config import Config
 from .logging import get_logger
 from .exceptions import StrategyNotImplemented
@@ -55,17 +57,18 @@ class StrategyBase():
 
     """
 
-    def __init__(self, config=None, samples=None):
+    def __init__(self, config=None, exclusions=None):
         self.messages = None
-        self.samples = samples
+        self.exclusions = exclusions
         self.config = config or Config()
 
     # Strategies:
     def compare_list_of_dicts(self, d1, d2):
         return [v for i,v in enumerate(d2) if v not in d1]
 
-    def compare_flat_dicts(self, d1, d2):
-        """Expects data in the format:
+
+    def compare_flat_dicts(self, d1, d2, tolerance=None):
+        """Expects data (d1, d2) in the format:
             {
             'key1', 'value1',
             'key2': 'value2',
@@ -74,8 +77,52 @@ class StrategyBase():
             ...
             }
 
+            tolerance: <float> is % difference for numeric values calculated as
+            differences.
+
+
         """
-        return [v for i,v in enumerate(d2) if v not in d1]
+        return diff(d1, d2, tolerance=tolerance)
+
+
+    @staticmethod
+    def format_change(diffs):
+
+        results = []
+
+        for diff in diffs:
+            ctype, element, details = diff
+
+            if ctype == "change":
+                results.append({'type': ctype, 'item': element, 'before': details[0], 'after': details[1]})
+            else:
+                for item in details:
+                    results.append({'type': ctype, 'item': item[0], 'content': item[1]})
+
+        return results
+
+
+    @staticmethod
+    def l2d(l1, l2, key_attr, content_attr):
+        """Turns a list of dicts into a dict based on given key attribute and content attribute.
+            parameters:
+                l1: <list> first list of dicts.
+                l2: <list> second list of dicts.
+                key_attr: <str> dict key to be used as key for new dict.
+                content_attr: <str> or <list> dict key(s) to be used as value for new dict.
+        """
+        if isinstance(content_attr, str):
+            d1 = {i[key_attr]:i[content_attr] for i in l1 if i[key_attr] and len(i[key_attr])}
+            d2 = {i[key_attr]:i[content_attr] for i in l2 if i[key_attr] and len(i[key_attr])}
+        elif isinstance(content_attr, list):
+            d1 = {i[key_attr]:{i[v] for v in content_attr} for i in l1 if i[key_attr] and len(i[key_attr])}
+            d2 = {i[key_attr]:{i[v] for v in content_attr} for i in l2 if i[key_attr] and len(i[key_attr])}
+        else:
+            raise NotImplementedError("`content_attr` can only be of type `str` or `list`")
+
+
+    return d1,d2
+
 
     def compare_data(self, data):
         raise NotImplementedError
